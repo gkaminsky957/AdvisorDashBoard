4dca60822d2f21132ece67491ad215b0146b871f
Gennady Kaminsky <gennady@theleaguestock.com>
2025-03-17 11:39:54 -0500

    Initial Commit

---
+++ AdvisorDashBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdvisorDashBoard
+//
+//  Created by Gennady Kaminsky on 3/17/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
