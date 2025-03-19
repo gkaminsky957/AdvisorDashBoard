1103e87b996443e8fd4dd75cc2eecdd048338f81
Gennady Kaminsky <gennady@theleaguestock.com>
2025-03-18 22:43:20 -0500

    Implemented account summary cell

--- AdvisorDashBoard/AccountsSummary/view/AccountSummaryTableViewCell.swift
+++ AdvisorDashBoard/AccountsSummary/view/AccountSummaryTableViewCell.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class AccountSummaryTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var accountName: UILabel!
+    @IBOutlet weak var accountNumber: UILabel!
+    @IBOutlet weak var custodianName: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        selectionStyle = .none
+        
+        let layer = self.layer
+        layer.cornerRadius = 3
+        layer.shadowRadius = 3
+        layer.shadowColor = UIColor.brown.cgColor
+        layer.shadowOffset = CGSize(width: 1, height : 1.0)
+        layer.shadowOpacity = 0.6
     }
     
     func setCell(model: AccountSummaryModel.AccountSummary) {
-        
+        accountName.text = model.name
+        accountNumber.text = model.number
+        custodianName.text = model.custodian
     }
 }
